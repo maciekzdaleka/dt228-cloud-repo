ab44953bc2761702c8f71637c871052f409de0e8
maciekzdaleka <c13470112@mydit.ie>
2015-09-30 15:35:44 +0000

    Another commit

--- lab3/maciek.py
+++ lab3/maciek.py
@@ -6,3 +6,17 @@ def palindrome (array):
 
     if min > max:
       return True
+    
+    a = array[min]
+    b = array[max]
+    
+    if a != b:
+      return False;
+    
+    min += 1
+    max -= 1
+    
+  words = ["Oxo", "OXO", "123454321", "ROTATOR2, "12345", "54321"]
+    
+    
+    
