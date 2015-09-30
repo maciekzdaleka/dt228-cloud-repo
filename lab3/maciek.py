74bcd6b50d86cbe7a190433dfd3947f2881148e7
maciekzdaleka <c13470112@mydit.ie>
2015-09-30 15:52:45 +0000

    It works teeeheeee

--- lab3/maciek.py
+++ lab3/maciek.py
@@ -6,17 +6,21 @@ def palindrome (array):
 
     if min > max:
       return True
-    
+
     a = array[min]
     b = array[max]
-    
+
     if a != b:
       return False;
-    
+
     min += 1
     max -= 1
-    
-  words = ["Oxo", "OXO", "123454321", "ROTATOR2, "12345", "54321"]
-    
-    
-    
+
+words = ["Oxo", "OXO", "123454321", "ROTATOR", "12345	54321", "omg"]
+
+for line in words:
+  if palindrome(line):
+    print("Palindrome yey:    ", line)
+  else:
+    print("Not palindrome:", line)
+
